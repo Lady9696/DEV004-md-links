8dec4cb5e22cb2f841a0251e08630ec768a820c2
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-05-04 18:22:42 -0500

    do while a medias

---
+++ test/data.js
@@ -0,0 +1,44 @@
+
+const fs = require('fs');
+const path = require('path');
+
+//hacer una funciòn para leer direcotrios
+const identificator = (routes) => {
+    //para identificar si es un directorio
+    fs.stat(routes, (err, stats) => {
+        if (err) {
+            reject(err)
+        } else {
+            if (!stats.isFile()) {
+                console.log('es un direcotiro');
+                do {
+                    filenames = fs.readdirSync(__dirname);
+                    filenames.forEach(files => {
+                        console.log(files, 'esta es el archivo del directorio');
+
+                    });
+
+                } while (routes = true) {
+
+                    console.log('es una fila', prueba);
+                    //se utiliza esto fs.readdirSync() method 
+                    //leerlo como directorio y me va entregra un array con las rutas de los archivos
+
+                }
+
+            } else {
+                console.log('es un archivo');
+            }
+        }
+
+
+    });
+
+
+}
+module.exports = {
+    identificator
+};
+module.exports.fs = require('fs');
+module.exports.path = require('path');
+
