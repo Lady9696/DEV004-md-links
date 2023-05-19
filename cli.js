305e7b3cfbe40431700bcc33039f047c326b970e
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-05-19 10:26:38 -0500

    utlima version

--- cli.js
+++ cli.js
@@ -3,21 +3,32 @@ const mdLinks = require("./index.js");
 //import { argv } from 'node:process';
 
 
-
-//routeUser = process.argv;
-
+// Aquì capturo la ruta del usuario
+pathInput = process.argv[2];
 
 
 //const arguments = process.argv;
 //console.log( arguments );
 const isValidate = process.argv;
-if(isValidate.includes('--validate')||isValidate.includes('-v')){
-  console.log({isValidate});
+/*if(isValidate.includes('--validate')||isValidate.includes('-v')){
+  //console.log({isValidate});
+  mdLinks(pathInput);
+.then(( result ) => {
+  [{ href, text, file }]
+
+  console.log('resultadoooooooooooooooooooooooooooooooooooooooooooooooooo',href);
+})
+.catch((error) => {
+   console.log(error)
+
+ });
  
 }
+//console.log(process);
+
 // llmar a mdLinks para que devuelva {´hrf, statu...}
 //console.log(`hello world${args}`);
-/*
+
 mdLinks('./test/README.md')
 .then(( result )=>{
   [{ href, text, file }]
