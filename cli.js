ce3685eb867df6de1a9fa5f0a1874cd9501b3c6a
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-05-25 12:33:17 -0500

    estilos completados

--- cli.js
+++ cli.js
@@ -31,13 +31,13 @@ pathInput = process.argv[2];
       .then((result) => {
         
         // Realiza acciones con el resultado
-       //console.log("\x1b[35m"+'Los resultados son:', result, '******************************');
+       console.log("\x1b[35m"+'Los resultados son:', result);
         // Llama a otra función y pasa el resultado como argumento
         
       })
       .catch((error) => {
         // Maneja los errores
-        console.error('Ocurrió un error:', error, '++++++++++++++++');
+        console.error("\x1B[41m"+'Ocurrió un error:', error);
       });
     
 
@@ -45,13 +45,13 @@ pathInput = process.argv[2];
       mdLinks(pathInput)
       .then((result) => {
         // Realiza acciones con el resultado
-       console.log('Total links:', result.length,);
+       console.log("\x1b[35m"+'Total links:',"\x1B[38;2;255;151;203m"+ result.length,);
         // Llama a otra función y pasa el resultado como argumento
         
       })
       .catch((error) => {
         // Maneja los errores
-        console.error('Ocurrió un error:', error.length, '++++++++++++++++');
+        console.error("\x1B[41m"+'Ocurrió un error:', error.length);
       });
 
     }else if( isValidate.includes('--stats--validate') || argv.includes('-s-v')) {
@@ -62,16 +62,16 @@ pathInput = process.argv[2];
       //console.log(totalLinks)
 
        const uniqueLinks = new Set(result.map(link => link.href)).size;// midiendo el tamaño de cuantos links unicos hay 
-       console.log(`Total: ${totalLinks}`);
-       console.log(`Unique: ${uniqueLinks}`);
+       console.log(`\x1b[35m Total: \x1B[38;2;255;151;203m${totalLinks}`);
+       console.log(`\x1b[35m Unique: \x1B[38;2;255;151;203m${uniqueLinks}`);
        //aqui puedo filtrar los links que tengan status fail
        // 
        const brokenLinks = (result.filter(link => link.ok==='fail').length);
-       console.log(`Broken: ${brokenLinks}`)
+       console.log(`\x1b[35m Broken: \x1B[38;2;255;151;203m${brokenLinks}`)
        //brokenlinks = uniqueLinks.filter( => miembro.posicion !== "developer")
       }) 
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("\x1B[41m"+'Error:', error);
       });
 
 
